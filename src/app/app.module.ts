0c619d8b4320acb27bdc5196b34a9a115137d469
xMatiSDx <maty_arias@hotamil.com>
2023-01-28 04:16:25 -0300

    working on navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { FontAwesomeModule, FaIconLibrary, FaConfig } from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faSignIn } from '@fortawesome/free-solid-svg-icons';
+
+import { IndexComponent } from './index/index/index.component';
+import { NavbarComponent } from './navbar/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    NgbDropdownModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(library: FaIconLibrary, faConfig: FaConfig) {
+    library.addIconPacks(fas, fab);
+    faConfig.fixedWidth = true;
+  };
+}
