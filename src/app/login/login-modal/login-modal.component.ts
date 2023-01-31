1de490d9e3203809e54647ce1b2e3b20b0a5e436
xMatiSDx <maty_arias@hotamil.com>
2023-01-31 01:24:20 -0300

    login modal done

--- src/app/login/login-modal/login-modal.component.ts
+++ src/app/login/login-modal/login-modal.component.ts
@@ -10,11 +10,26 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class LoginModalComponent {
 
-  login = new FormGroup ({
+  loginForm = new FormGroup ({
     email: new FormControl(''),
     password: new FormControl(''),
   })
 
   constructor(public activeModal:NgbActiveModal, private formBuilder: FormBuilder) {}
+
+  onEnviar(event: Event){
+    // Detenemos la propagación o ejecución del compotamiento submit de un form
+    event.preventDefault; 
+ 
+    if (this.loginForm.valid){
+      // Llamamos a nuestro servicio para enviar los datos al servidor
+      // También podríamos ejecutar alguna lógica extra
+      alert("Todo salio bien ¡Enviar formulario!")
+    }else{
+      // Corremos todas las validaciones para que se ejecuten los mensajes de error en el template     
+      this.loginForm.markAllAsTouched(); 
+    }
+ 
+  }
 
 }
