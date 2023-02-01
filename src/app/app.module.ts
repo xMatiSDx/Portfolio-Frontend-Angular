b3f9e49e66dad622833eb52e4305ce7a598d1c1d
xMatiSDx <maty_arias@hotamil.com>
2023-01-31 23:45:10 -0300

    RouterLink Done + ActiveLinks css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterLinkActive } from '@angular/router'
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
-
+import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index/index.component';
 import { NavbarComponent } from './navbar/navbar/navbar.component';
 import { CoverComponent } from './cover/cover/cover.component';
 import { EditButtonComponent } from './buttons/edit-button/edit-button.component';
 import { LoginButtonComponent } from './buttons/login-button/login-button.component';
 import { LoginModalComponent } from './login/login-modal/login-modal.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -28,8 +30,11 @@ import { LoginModalComponent } from './login/login-modal/login-modal.component';
   imports: [
     BrowserModule,
     NgbModule,
+    ReactiveFormsModule,
+    RouterLinkActive,
     NgbDropdownModule,
     FontAwesomeModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login-modal/login-modal.component.ts
+++ src/app/login/login-modal/login-modal.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
@@ -10,18 +10,42 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class LoginModalComponent {
 
-  loginForm = new FormGroup ({
-    email: new FormControl(''),
-    password: new FormControl(''),
-  })
+  // loginForm = new FormGroup ({
+  //   email: new FormControl(''),
+  //   password: new FormControl(''),
+  // })
 
-  constructor(public activeModal:NgbActiveModal, private formBuilder: FormBuilder) {}
+  loginForm:FormGroup;
+
+  constructor(public activeModal:NgbActiveModal, private formBuilder: FormBuilder) {
+    this.loginForm= this.formBuilder.group({
+      password:['',[Validators.required, Validators.minLength(8)]],
+      email:['',[Validators.required, Validators.email]],
+   })
+  }
+
+  get password(){
+    return this.loginForm.get("password");
+  }
+ 
+  get email(){
+   return this.loginForm.get("email");
+  }
+
+  get passwordValid(){
+    return this.password?.touched && !this.password?.valid;
+  }
+
+  get emailValid() {
+    return this.email?.touched && !this.email?.valid;
+  }
 
   onEnviar(event: Event){
     // Detenemos la propagación o ejecución del compotamiento submit de un form
     event.preventDefault; 
  
     if (this.loginForm.valid){
+      
       // Llamamos a nuestro servicio para enviar los datos al servidor
       // También podríamos ejecutar alguna lógica extra
       alert("Todo salio bien ¡Enviar formulario!")
